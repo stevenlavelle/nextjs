f4220169562fc6987c5a535f6c3a8193fc3e2121
Steven Lavelle <stevenlavelle@steven-mbp.attlocal.net>
2021-08-23 20:34:05 -0400

    second commit

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
-import Link from "next/link";
 
 const name = "Steven Lavelle";
 export const siteTitle = "Next.js Sample Website";
